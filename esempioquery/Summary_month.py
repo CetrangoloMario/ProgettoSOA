b681128d8de0a6d31fb87d063a2dc741f12bfe46
Mario Cetrangolo <55892147+CetrangoloMario@users.noreply.github.com>
2021-06-29 22:33:03 +0200

    New Branch

--- esempioquery/Summary_month.py
+++ esempioquery/Summary_month.py
@@ -1,22 +1,18 @@
-import csv
-import glob
 import sys
-from pyspark.sql import SparkSession, DataFrame
 
-#spark-submit --master local --executor-memory 5G --num-executors 4 --executor-cores 3
- #\Users\manlio\Desktop\ScriptConcatenaCSV.py
- #/Users/manlio/Desktop/COVID19_Tweets_Dataset/Summary_Details/2020_01"
- #/Users/manlio/Desktop/COVID19_Tweets_Dataset/AllSummaryDetails1.csv
+from pyspark.sql import *
+from pyspark.sql.functions import *
 
 spark = SparkSession.builder.appName("esempio").getOrCreate()
 
-pathcsv = sys.argv[1]
-pathOutput =sys.argv[2]
-
+pathcsv= sys.argv[1]
+#pathOutput =sys.argv[2]
 print(pathcsv)
-print(pathOutput)
+#print(pathOutput)
 
-
-
+dfTopLang= spark.read.option("inferSchema","true").option("header","true").csv(pathcsv)
+super=dfTopLang.groupby(month("Date Created"))
+super.sum("Retweets","Likes")
+super.write.csv("/user/soa/dataset/ciao.csv", header=False)
 
 print("OK")
