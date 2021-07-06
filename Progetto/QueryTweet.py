978b0c3076d52715a40a75f080b7e1a1e8f7fca7
Mario Cetrangolo <55892147+CetrangoloMario@users.noreply.github.com>
2021-07-06 16:40:21 +0200

    QueryTweet.py ok

--- Progetto/QueryTweet.py
+++ Progetto/QueryTweet.py
@@ -7,52 +7,69 @@ import pyspark.sql.functions as F
 spark = SparkSession.builder.appName("Query_su_Tweet_Covid19").getOrCreate()
 
 listmese="Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"
+listgiorni ="Jan 22", "Mar 11","Dec 14", "Feb 21", "Oct 28"
+
+
+def topAllDay(*dataframe ):
+    dfDetails,dfSentiment,dfHastag  = dataframe
+    sj = dfHastag.join(dfSentiment, "Tweet_ID", 'inner')
+    dfJoin = sj.join(dfDetails, "Tweet_ID", 'inner')
+    for i in listgiorni:
+        try:
+            super = dfJoin.filter(instr(dfJoin["Date Created"], i) >= 1)
+            topSentiment(super)
+            topHashtag(super)
+            topLang(super)
+
+        except:
+            print("Giorno saltato: " + i)
+            continue
+
 
 
 def topLang(dataframe):
-    super = dataframe.groupby("Language").count().orderBy("count", ascending=False)
-    super.write.csv("/Users/cetra/Desktop/risultati/topLang.csv", header=False)
+    super = dataframe.groupby("Language").count().orderBy("count", ascending=False).show()
+    super.write.csv("/Users/cetra/Desktop/risultati/topLang.csv",mode="append", header=False)
 
 def topHashtag(dataframe):
-    super = dataframe.groupby("Hashtag").count().orderBy("count", ascending=False)
-    super.write.csv("/Users/cetra/Desktop/risultati/topHashtag.csv", header=False)
+    super = dataframe.groupby("Hashtag").count().orderBy("count", ascending=False).show()
+    super.write.csv("/Users/cetra/Desktop/risultati/topHashtag.csv",mode="append", header=False)
 
 def topSentiment(dataframe):
-    super = dataframe.groupby("Sentiment_Label").count().orderBy("count", ascending=False)
-    super.write.csv("/Users/cetra/Desktop/risultati/topSentiment.csv", header=False)
+    super = dataframe.groupby("Sentiment_Label").count().orderBy("count", ascending=False).show()
+    super.write.csv("/Users/cetra/Desktop/risultati/topSentiment.csv",mode="append", header=False)
 
 
 
 def topLangMonth(dataframe):
     for i in listmese:
         super = dataframe.filter(instr(dataframe["Date Created"], i)>=1).groupby("Language").count().withColumnRenamed("count",i).orderBy(i,ascending=False)
-        super.write.csv("/Users/cetra/Desktop/risultati/topLangMonth_"+i+".csv", header=False)
+        super.write.csv("/Users/cetra/Desktop/risultati/topLangMonth_"+i+".csv",mode="append", header=False)
 
 def topSentimentMonth(*dataframe):
     dfDetails, dfSentiment = dataframe
     dfjoin = dfSentiment.join(dfDetails, "Tweet_ID", 'inner')
     for i in listmese:
         super = dfjoin.filter(instr(dfjoin["Date Created"], i)>=1).groupby("Sentiment_Label").count().withColumnRenamed("count",i).orderBy(i,ascending=False)
-        super.write.csv("/Users/cetra/Desktop/risultati/topSentimentMonth_"+i+".csv", header=False)
+        super.write.csv("/Users/cetra/Desktop/risultati/topSentimentMonth_"+i+".csv",mode="append", header=False)
 
 def topHashtagMonth(*dataframe):
     dfDetails, dfHashtag = dataframe
     dfjoin = dfHashtag.join(dfDetails, "Tweet_ID", 'inner')
     for i in listmese:
         super = dfjoin.filter(instr(dfjoin["Date Created"], i)>=1).groupby("Hashtag").count().withColumnRenamed("count",i).orderBy(i,ascending=False)
-        super.write.csv("/Users/cetra/Desktop/risultati/topHashtagMonth_"+i+".csv", header=False)
+        super.write.csv("/Users/cetra/Desktop/risultati/topHashtagMonth_"+i+".csv",mode="append", header=False)
 
 
 def avgTweetsMonth(dataframe):
     for i in listmese:
         super = dataframe.filter(instr(dataframe["Date Created"], i)>=1).select("Tweet_ID","Retweets").agg({"Tweet_ID":"avg","Retweets":"avg"})
-        super.write.csv("/Users/cetra/Desktop/risultati/avgTweetsMonth_"+i+".csv", header=False)
+        super.write.csv("/Users/cetra/Desktop/risultati/avgTweetsMonth_"+i+".csv",mode="append", header=False)
 
-def totTweets_Retweets_Month(dataframe:pyspark.sql.dataframe.DataFrame):
+def totTweets_Retweets_Month(dataframe):
     for i in listmese:
-        super = dataframe.filter(instr(dataframe["Date Created"], i)>=1).select("Tweet_ID","Retweets").agg(F.count("Tweet_ID"),F.sum("Retweets")).show()
-
-        #super.write.csv("/Users/cetra/Desktop/risultati/topLangMonth_"+i+".csv", header=False)
+        super = dataframe.filter(instr(dataframe["Date Created"], i)>=1).select("Tweet_ID","Retweets").agg(F.count("Tweet_ID"),F.sum("Retweets"))
+        super.write.csv("/Users/cetra/Desktop/risultati/topLangMonth_"+i+".csv",mode="append", header=False)
 
 
 
@@ -78,11 +95,11 @@ def maxLikeSentiment(*dataframe):
     maxNegative = super.filter("Sentiment_Label == 'negative'").select(F.max("Likes")).withColumnRenamed("max(Likes)", "MaxLikeNegative")  # .show()
     maxNeutral = super.filter("Sentiment_Label == 'neutral'").select(F.max("Likes")).withColumnRenamed("max(Likes)", "MaxLikeNeutral")  # .show()
 
-    maxPositive.write.csv("/Users/cetra/Desktop/risultati/maxLikeSentimentPositive.csv", header=True)
-    maxNegative.write.csv("/Users/cetra/Desktop/risultati/maxLikeSentimentNegative.csv", header=True)
-    maxNeutral.write.csv("/Users/cetra/Desktop/risultati/maxLikeSentimentNeturral.csv", header=True)
+    maxPositive.write.csv("/Users/cetra/Desktop/risultati/maxLikeSentimentPositive.csv",mode="append", header=True)
+    maxNegative.write.csv("/Users/cetra/Desktop/risultati/maxLikeSentimentNegative.csv",mode="append", header=True)
+    maxNeutral.write.csv("/Users/cetra/Desktop/risultati/maxLikeSentimentNeturral.csv",mode="append", header=True)
 
-def maxHashtagSentiment(*dataframe:pyspark.sql.dataframe.DataFrame):
+def maxHashtagSentiment(*dataframe):
     dfHastag, dfSentiment, dfDetails=dataframe
     sj=dfHastag.join(dfSentiment,"Tweet_ID",'inner')
     dfJoin=sj.join(dfDetails,"Tweet_ID",'inner')
@@ -97,9 +114,9 @@ def maxHashtagSentiment(*dataframe:pyspark.sql.dataframe.DataFrame):
             maxNegative = dfJoin.filter("Sentiment_Label == 'negative'").where("Hashtag =='"+maxHashtag+"'").agg(F.count("Tweet_ID")).withColumnRenamed("count(Tweet_ID)","Negative_max_Hashtag_"+i)
             maxNeutral = dfJoin.filter("Sentiment_Label == 'neutral'").where("Hashtag == '"+maxHashtag+"'").agg(F.count("Tweet_ID")).withColumnRenamed("count(Tweet_ID)","Neutral_max_Hashtag_"+i)
 
-            maxPositive.write.csv("/Users/cetra/Desktop/risultati/Positive_max_Hashtag_"+i+".csv", header=True)
-            maxNegative.write.csv("/Users/cetra/Desktop/risultati/Negative_max_Hashtag_"+i+".csv", header=True)
-            maxNeutral.write.csv("/Users/cetra/Desktop/risultati/Neutral_max_Hashtag_"+i+".csv", header=True)
+            maxPositive.write.csv("/Users/cetra/Desktop/risultati/Positive_max_Hashtag_"+i+".csv",mode="append", header=True)
+            maxNegative.write.csv("/Users/cetra/Desktop/risultati/Negative_max_Hashtag_"+i+".csv",mode="append", header=True)
+            maxNeutral.write.csv("/Users/cetra/Desktop/risultati/Neutral_max_Hashtag_"+i+".csv",mode="append", header=True)
         except :
             print("Mese saltato: "+i)
             continue
@@ -130,6 +147,7 @@ def main():
     #summaryMonth(dfDetails)
     #maxLikeSentiment(dfDetails,dfSentiment)
     #maxHashtagSentiment(dfHashtag,dfSentiment,dfDetails)
+    topAllDay(dfDetails,dfSentiment,dfHashtag)
 
 
 
